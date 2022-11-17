f8fe75994d13af5e9bb1ae0c4f36758d1413d230
fall2022mth9815 <118473511+fall2022mth9815@users.noreply.github.com>
2022-11-17 13:20:17 -0500

    Add files via upload

---
+++ HW1/Node.h
@@ -0,0 +1,18 @@
+#ifndef Node_h
+#define Node_h
+
+template <typename T>
+class Node {
+public:
+
+	T value;
+	Node<T>* next_node;
+
+	Node(Node<T>* next_node_) : next_node(next_node_) {}
+	Node(T value_, Node<T>* next_node_ = nullptr) : value(value_), next_node(next_node_) {}
+	virtual Node<T>* get_next() {
+		return next_node;
+	}
+};
+
+#endif
